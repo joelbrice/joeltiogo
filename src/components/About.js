c4ca362e901b07cb8525153af12c238b57ecc1c9
Joel Tiogo <tiogojoel@gmail.com>
2024-07-20 17:05:02 +0200

    about.js

--- src/components/About.js
+++ src/components/About.js
@@ -162,7 +162,7 @@ function About()
 {
   return (
     <div className="bg-gray-100">
-       
+
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="lg:text-center">
         <h2 className="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl text-center">
